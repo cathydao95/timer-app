6adea860df08890c6c51f93aecf45d0edca7d5cc
Cathy Dao <cathydao95@gmail.com>
2022-05-13 00:03:36 -0700

    styles to timer

--- timer.js
+++ timer.js
@@ -3,13 +3,13 @@ class Timer {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
-    // to show that callbacksare optional:
+
     if (callbacks) {
       this.onStart = callbacks.onStart;
       this.onTick = callbacks.onTick;
       this.onComplete = callbacks.onComplete;
     }
-    // why need to put this before start.bind? and why does eventlistener go into constructor?
+
     startButton.addEventListener("click", this.start);
     pauseButton.addEventListener("click", this.pause);
   }
@@ -19,7 +19,7 @@ class Timer {
       this.onStart(this.timeRemaining);
     }
     this.tick();
-    // Changed const interval to this.interval to allow pause method to access timer???
+
     this.interval = setInterval(this.tick, 50);
   };
 
@@ -29,12 +29,6 @@ class Timer {
   };
 
   tick = () => {
-    //   const timeRemaining = parseFloat(this.duration.value); rewriting using the get timeRemaining to get:
-    // const timeRemaining = this.timeRemaining;
-    // // this.durationInput.value = timeRemaining - 1; rewriting using the set timeRemaining to get:
-    // this.timeRemaining = timeRemaining - 1;
-    // reducing everything above and simplyfing to
-
     if (this.timeRemaining <= 0) {
       this.pause();
       if (this.onComplete) {
